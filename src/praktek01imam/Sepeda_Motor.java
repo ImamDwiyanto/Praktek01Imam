fcae7f7bfcce2b2d3b33a15ce0cf419a918327e9
Imam Dwiyanto <imamdwiyanto21@gmail.com>
2018-04-06 13:34:14 +0700

    Membuat atribut dan behavior

--- src/praktek01imam/Sepeda_Motor.java
+++ src/praktek01imam/Sepeda_Motor.java
@@ -10,5 +10,17 @@ package praktek01imam;
  * @author ACER-PC
  */
 public class Sepeda_Motor {
+    String Merk;
+    String Warna;
+    String CC;
+    String Velg;
+    String Ban;
     
+    void cetakInfo() {
+        System.out.println("Merk  :"+Merk);
+        System.out.println("Warna :"+Warna);
+        System.out.println("CC    :"+CC);
+        System.out.println("Velg  :"+Velg);
+        System.out.println("Ban   :"+Ban);
+    }
 }
