99ff0d82ba9c6a9e0f9cad76326a6edff6e17a33
Imam Dwiyanto <imamdwiyanto21@gmail.com>
2018-04-06 13:40:22 +0700

    Mengisi nilai atribut

--- src/praktek01imam/Sepeda_MotorAKsi.java
+++ src/praktek01imam/Sepeda_MotorAKsi.java
@@ -12,5 +12,11 @@ package praktek01imam;
 public class Sepeda_MotorAKsi {
     public static void main(String[] args) {
         Sepeda_Motor Bapak = new Sepeda_Motor();
+        
+        Bapak.Merk="Yamaha";
+        Bapak.Warna="Merah";
+        Bapak.CC="150cc";
+        Bapak.Velg="Bintang";
+        Bapak.Ban="Tubles";
     }
 }
